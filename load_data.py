1f57478f2a58e558d0f7b3a6c588e3a3be5fe6da
Arnab Ghose <arnab.ghose128@gmail.com>
2018-07-29 12:27:52 +0530

    Update

--- load_data.py
+++ load_data.py
@@ -1,36 +1,33 @@
 import numpy as np
 import struct as st
 
-def load_data(FEATURES, LABELS):
-	train_features = FEATURES
-	train_labels = LABELS
-
+def load_data(features_file, labels_file):
 	#Features Dimensions
-	with open(train_features, "rb") as tf:
+	with open(features_file, "rb") as tf:
 		tf.seek(4)
-		file_features = tf.read(12) 
+		file_f = tf.read(12) 
 
 	#Label Dimensions
-	with open(train_labels, "rb") as tl:
+	with open(labels_file, "rb") as tl:
 		tl.seek(4)
-		file_labels = tl.read(4)
+		file_l = tl.read(4)
 
-	features_dim  = st.unpack(">III", file_features) #(no_of_images, nRows, nColumns)
-	labels_dim = st.unpack(">I", file_labels) #(no_of_images)
+	features_dim  = st.unpack(">III", file_f) #(no_of_images, nRows, nColumns)
+	labels_dim = st.unpack(">I", file_l) #(no_of_images)
 
 	number_of_images = features_dim[0]
 	rows = features_dim[1]
 	cols = features_dim[2]
 
 	#Labels data
-	with open(train_labels, "rb") as a:
+	with open(labels_file, "rb") as a:
 		a.seek(8)
 		labels = a.read()
 
 	labels = np.array(list(labels))
 
 	#Features data
-	with open(train_features, "rb") as b:
+	with open(features_file, "rb") as b:
 		b.seek(16)
 		features = b.read()
 
@@ -48,4 +45,4 @@ TESTING_LABELS = "mnist/t10k-labels.idx1-ubyte"
 
 f,l = load_data(TESTING_FEATURES, TESTING_LABELS)
 print(l.shape)
-'''
+''
